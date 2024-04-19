5a601499137f891e33fd853fbf20427b4db7634b
Andreasgura <andre-56@hotmail.it>
2024-04-19 17:51:46 +0200

    modal component pt 1

--- src/assets/components/ModalCard.vue
+++ src/assets/components/ModalCard.vue
@@ -1,12 +1,34 @@
 <template>
-   <button @click="open = true">Apri modale</button>
+    <button @click="open = true">More info</button>
 
-<Teleport to="body">
-  
-    <p>Ciao dalla Modale!</p>
-    
- 
-</Teleport>
+    <Teleport to="body">
+        <div class="modal fade " :class="open ? 'show' : ''" :style="open ? 'display: block' : ''" id="modal" tabindex="-1"
+            aria-labelledby="exampleModalLongTitle" aria-modal="true" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLongTitle">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                            @click="open = false"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>This is some placeholder content to show the scrolling behavior for modals. Instead of
+                            repeating the text in the modal, we use an inline style to set a minimum height, thereby
+                            extending the length of the overall modal and demonstrating the overflow scrolling. When
+                            content becomes longer than the height of the viewport, scrolling will move the modal as
+                            needed.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                            @click="open = false">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Teleport>
+
+
+
 </template>
 
 <script>
@@ -14,21 +36,17 @@ export default {
     name: 'ModalCard',
     data() {
         return {
-            open: true
+            open: false
         }
     }
 }
 </script>
 
-<style lang="scss" >
-.modal {
-  position: fixed;
-  z-index: 999;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  
+<style lang="scss" scoped>
+#modal {
+
+    background-color: rgba(0, 0, 0, 0.049);
+
 }
+
 </style>
